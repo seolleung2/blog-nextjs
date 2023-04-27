becb826beb53377fb2e36a265e51ebe8ee5e331b
seolleung2 <seolleung22@gmail.com>
2023-04-27 16:56:32 +0900

    fix: react-rotating-text 패키지 설치 및 적용 - react-rotating-text 패키지 설치 - 메인 배너에 적용 (텍스트 애니메이션 효과)

--- src/components/layout/banner/index.tsx
+++ src/components/layout/banner/index.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import ReactRotatingText from 'react-rotating-text';
 import Button from '@components/button';
 
 export default function PageBanner() {
@@ -10,9 +11,15 @@ export default function PageBanner() {
 
   return (
     <div className="flex justify-center md:space-x-4">
-      <div className="flex flex-col items-center justify-center md:items-start">
+      <div className="flex w-full flex-col items-center justify-center md:w-2/3 md:items-start">
         <h2 className="mb-4 text-2xl font-extrabold text-slate-800 md:text-3xl lg:text-5xl">
-          Hello, I&apos;m <span className="text-purple-500">Kwangmook</span>.
+          Hello, I&apos;m{' '}
+          <span className="text-purple-500">
+            <ReactRotatingText
+              items={['Kwangmook', 'Creative Dev', 'Selt-Confident']}
+            />
+          </span>
+          .
         </h2>
         <h2 className="mb-8 text-2xl font-extrabold text-slate-800 md:text-3xl lg:text-5xl">
           Frontend Developer
@@ -20,7 +27,7 @@ export default function PageBanner() {
         <p className="mb-7 text-lg font-bold text-slate-500 md:text-xl lg:text-2xl">
           Specialized in NextJS and React
         </p>
-        <div className="space-x-4">
+        <div className="flex flex-wrap justify-center gap-4">
           <Button
             text="Contact Me"
             className="rounded-3xl"
