909522871391cf5f81024c76ab6948461901693e
seolleung2 <seolleung22@gmail.com>
2023-06-01 22:36:58 +0900

    fix: React Errorboundary 를 통한 커스텀 에러 (/posts/[slug] 내) 페이지 생성 참조 URL : https://nextjs.org/docs/app/building-your-application/routing/error-handling

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+  fallback: ReactNode;
+};
+type State = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(error: Error): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
