89b8830a36182684cbea4c205d9e20ebbecc84eb
seolleung2 <seolleung22@gmail.com>
2023-06-04 16:39:19 +0900

    fix: BlogNavigator 코드 분리작업 > Detail 컴포넌트로 분리

---
+++ src/components/navigator/Detail.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Image from 'next/image';
+import { MdSkipPrevious, MdSkipNext } from 'react-icons/md';
+import { Blog } from '@interfaces/Blog';
+import classNames from 'classnames';
+
+type Props = {
+  handleMovePage: () => void;
+  navData: Blog;
+  type: 'prev' | 'next';
+};
+
+export default function Detail({ navData, handleMovePage, type }: Props) {
+  return (
+    <div
+      className={classNames(
+        'group relative flex h-16 w-1/2 cursor-pointer items-center space-x-2 rounded-md bg-slate-200 p-3 font-medium shadow-lg hover:bg-purple-500 hover:text-white md:h-28 md:bg-transparent md:font-bold md:text-white md:hover:bg-transparent',
+        type === 'prev' ? 'justify-start' : 'justify-end'
+      )}
+      onClick={handleMovePage}
+    >
+      {type === 'prev' && (
+        <MdSkipPrevious className="block text-xl md:w-1/6 md:text-3xl" />
+      )}
+      <span className="md:hidden">
+        {type === 'prev' ? '이전' : '다음'} 블로그로
+      </span>
+      <div
+        className={classNames(
+          'line-clamp-2 hidden flex-col space-y-2 md:flex',
+          type === 'prev' ? 'items-start' : 'items-end'
+        )}
+      >
+        <span className="w-fit rounded-lg bg-slate-200 px-2 py-0.5 text-black">
+          {type === 'prev' ? '이전글' : '다음글'}
+        </span>
+        <p className="h-12">{navData?.title || ''}</p>
+      </div>
+      {type === 'next' && (
+        <MdSkipNext className="block text-xl md:w-1/6 md:text-3xl" />
+      )}
+      <Image
+        src={navData?.coverImage || '/images/default-cover.jpg'}
+        alt="post-thumbnail"
+        width={120}
+        height={80}
+        className="absolute right-0 -z-10 hidden h-28 w-full rounded-md object-cover brightness-50 group-hover:brightness-75 md:block"
+      />
+    </div>
+  );
+}
--- src/components/navigator/index.tsx
+++ src/components/navigator/index.tsx
@@ -2,9 +2,8 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import { MdSkipPrevious, MdSkipNext } from 'react-icons/md';
 import { Blog } from '@interfaces/Blog';
+import Detail from './Detail';
 
 type Props = {
   prev: Blog | null;
@@ -25,48 +24,10 @@ export default function BlogNavigator({ prev, next }: Props) {
   return (
     <section className="flex space-x-4">
       {prev && (
-        <div
-          className="group relative flex h-16 w-1/2 cursor-pointer items-center justify-start space-x-2 rounded-md bg-slate-200 p-3 font-medium shadow-lg hover:bg-purple-500 hover:text-white md:h-28 md:bg-transparent md:font-bold md:text-white md:hover:bg-transparent"
-          onClick={moveToPrevPage}
-        >
-          <MdSkipPrevious className="block text-xl md:w-1/6 md:text-3xl" />
-          <span className="md:hidden">이전 블로그로</span>
-          <div className=" line-clamp-2 hidden flex-col items-start space-y-2 md:flex">
-            <span className="w-fit rounded-lg bg-slate-200 px-2 py-0.5 text-black">
-              이전글
-            </span>
-            <p className="h-12">{prev?.title || ''}</p>
-          </div>
-          <Image
-            src={prev?.coverImage || '/images/default-cover.jpg'}
-            alt="post-thumbnail"
-            width={120}
-            height={80}
-            className="absolute right-0 -z-10 hidden h-28 w-full rounded-md object-cover brightness-50 group-hover:brightness-75 md:block"
-          />
-        </div>
+        <Detail handleMovePage={moveToPrevPage} navData={prev} type="prev" />
       )}
       {next && (
-        <div
-          className="group relative flex h-16 w-1/2 cursor-pointer items-center justify-end space-x-2 rounded-md bg-slate-200 p-3 font-medium shadow-lg hover:bg-purple-500 hover:text-white md:h-28 md:bg-transparent md:font-bold md:text-white md:hover:bg-transparent"
-          onClick={moveToNextPage}
-        >
-          <span className="md:hidden">다음 블로그로</span>
-          <div className=" line-clamp-2 hidden flex-col items-end space-y-2 md:flex">
-            <span className="w-fit rounded-lg bg-slate-200 px-2 py-0.5 text-black">
-              다음글
-            </span>
-            <p className="h-12">{next?.title || ''}</p>
-          </div>
-          <Image
-            src={next?.coverImage || '/images/default-cover.jpg'}
-            alt="post-thumbnail"
-            width={120}
-            height={80}
-            className="absolute right-0 -z-10 hidden h-28 w-full rounded-md object-cover brightness-50 group-hover:brightness-75 md:block"
-          />
-          <MdSkipNext className="block text-xl md:w-1/6 md:text-3xl" />
-        </div>
+        <Detail handleMovePage={moveToNextPage} navData={next} type="next" />
       )}
     </section>
   );
