e8762f81dce94fffe5195856652e32b6991ff349
seolleung2 <seolleung22@gmail.com>
2023-06-08 22:19:59 +0900

    add: 구글 서치 콘솔 등록을 위한 sitemap.xml 파일 생성 - next-sitemap 설치 및 사이트맵 생성 (next-sitemap.config.js) - package.json yarn build 시 커맨드 수정 - public 하위 경로에 robots.txt, sitemap.xml, sitemap-0.xml 생성 및 gitignore 등록 - Google Search Console 에서 받은 google-site-verification code 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: 'https://dotorimook-log.vercel.app/',
+  generateRobotsTxt: true,
+  exclude: ['/admin', '/login'],
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.5,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
 import './globals.css';
 import { Open_Sans } from 'next/font/google';
+import { Metadata } from 'next';
 import classNames from 'classnames';
 import { PageHeader, PageFooter } from '@components/layout';
+import Head from 'next/head';
 
 const sans = Open_Sans({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: {
     default: '도토리정의 DevLog',
     template: '도토리정의 DevLog | %s',
   },
   description: 'Frontend Dev 도토리정의 블로그',
+  verification: {
+    google: 'h5hXfhgh2YyYCX6iW7D3Bwbw9z4pIU5ypnJg2sbX-ZM',
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +25,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={classNames(sans.className, 'scroll-smooth')}>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="h5hXfhgh2YyYCX6iW7D3Bwbw9z4pIU5ypnJg2sbX-ZM"
+        />
+      </Head>
       <body className="flex min-h-screen flex-col bg-light selection:bg-purple-400 selection:text-white">
         <PageHeader />
         <main className="mx-auto w-full max-w-7xl grow p-8">{children}</main>
