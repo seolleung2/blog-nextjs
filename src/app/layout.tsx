b66ae10b7597c31666d7d50b0df3945df3bb3e7a
seolleung2 <seolleung22@gmail.com>
2023-06-08 22:24:20 +0900

    fix: App/layout.tsx 에서 불필요 코드 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Open_Sans } from 'next/font/google';
 import { Metadata } from 'next';
 import classNames from 'classnames';
 import { PageHeader, PageFooter } from '@components/layout';
-import Head from 'next/head';
 
 const sans = Open_Sans({ subsets: ['latin'] });
 
@@ -25,12 +24,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={classNames(sans.className, 'scroll-smooth')}>
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="h5hXfhgh2YyYCX6iW7D3Bwbw9z4pIU5ypnJg2sbX-ZM"
-        />
-      </Head>
       <body className="flex min-h-screen flex-col bg-light selection:bg-purple-400 selection:text-white">
         <PageHeader />
         <main className="mx-auto w-full max-w-7xl grow p-8">{children}</main>
