61927bc88734a6f975cb6d416a6ea7c34497f20e
seolleung2 <seolleung22@gmail.com>
2023-06-25 21:02:16 +0900

    fix: 블로그 Table Of Contents 선택 시 컨텐츠 heading 까지 보여지도록 수정 - TOC 컴포넌트 코드 수정, rootMargin 값 적용 - 전역 CSS 에 마크다운 영역의 h2, h3 에 scroll-margin-top 적용 (사파리는 padding, margin 적용)

--- src/components/toc/index.tsx
+++ src/components/toc/index.tsx
@@ -4,27 +4,30 @@ import classNames from 'classnames';
 import { useEffect, useState } from 'react';
 
 export default function TOC() {
-  // 목차 리스트 ( index: 목차, size: 목차의 크기 ( h1~h6는 크기를 다르게 렌더링해주기 위함 ) )
   const [indexList, setIndexList] = useState<{ index: string; size: number }[]>(
     []
   );
-
-  // 현재 보이는 목차 ( 강조 표시 해주기 위함 )
   const [currentIndex, setCurrentIndex] = useState<string>('');
 
   useEffect(() => {
-    // 1. <main> 내부에서만 목차를 만들거라서 <main> 선택
-    // 2. <h2>, <h3> 찾기 ( h4 ~ h6는 사용 x )
-    const hNodeList = document
-      .querySelector('main')
-      ?.querySelectorAll('h2, h3') as NodeListOf<Element>;
+    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setCurrentIndex(entry.target.id);
+        }
+      });
+    };
 
-    // IntersectionObserver들이 들어갈 배열 ( 이벤트 해제를 위해 )
+    const options = {
+      rootMargin: '-64px 0px -40% 0px',
+      threshold: 1,
+    };
+
+    const hNodeList = document.querySelectorAll('main h2, main h3');
+
     const IOList: IntersectionObserver[] = [];
-    let IO: IntersectionObserver;
 
-    [...hNodeList].forEach((node) => {
-      // 목차 내용이랑 사이즈 구해서 저장
+    hNodeList.forEach((node) => {
       const index = node.textContent as string;
       const size = (+node.nodeName[1] - 1) * 20;
       setIndexList((prev) => {
@@ -32,33 +35,17 @@ export default function TOC() {
         return [...prev, { index, size }];
       });
 
-      // 3. 각 <h*>에 id로 현재 컨텐츠 내용 추가
       node.id = index;
 
-      // 5. 화면에 보이면 강조되도록 "IntersectionObserver" 등록
-      IO = new IntersectionObserver(
-        ([
-          {
-            isIntersecting,
-            target: { textContent },
-          },
-        ]) => {
-          if (!isIntersecting) return;
-          setCurrentIndex(textContent!);
-        },
-        {
-          // rootMargin: '-64px 0px -40% 0px',
-          threshold: 1,
-        }
-      );
-      IO.observe(node);
+      const observer = new IntersectionObserver(observerCallback, options);
+      observer.observe(node);
 
-      // 이벤트 해제를 위해 등록
-      IOList.push(IO);
+      IOList.push(observer);
     });
 
-    // 이벤트 해제
-    return () => IOList.forEach((IO) => IO.disconnect());
+    return () => {
+      IOList.forEach((observer) => observer.disconnect());
+    };
   }, []);
 
   return (
@@ -73,6 +60,7 @@ export default function TOC() {
           <li
             key={index}
             style={{
+              // marginTop: size > 0 ? size / 2 + 'px' : '',
               paddingLeft: size + 'px',
               fontSize: 17 - size / 12 + 'px',
             }}
