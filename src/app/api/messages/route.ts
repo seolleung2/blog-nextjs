0195906efca979b08e11e45c9e6d03d6b95e71d7
seolleung2 <seolleung22@gmail.com>
2023-07-10 23:09:44 +0900

    add : message POST 요청 연동 (/api/messages)

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,8 +1,10 @@
 import { prisma } from '@service/lib/prisma';
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET() {
-  const messages = await prisma.message.findMany();
+  const messages = await prisma.message.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
 
   const JSON_RESPONSE = {
     status: 'success',
@@ -11,4 +13,36 @@ export async function GET() {
     messages,
   };
   return NextResponse.json(JSON_RESPONSE);
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const json = await request.json();
+
+    const feedback = await prisma.message.create({
+      data: json,
+    });
+
+    const JSON_RESPONSE = {
+      status: 'success',
+      data: {
+        feedback,
+      },
+    };
+
+    return new NextResponse(JSON.stringify(JSON_RESPONSE), {
+      status: 201,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: any) {
+    const ERROR_RESPONSE = {
+      status: 'error',
+      message: error.message,
+    };
+
+    return new NextResponse(JSON.stringify(ERROR_RESPONSE), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
 }
